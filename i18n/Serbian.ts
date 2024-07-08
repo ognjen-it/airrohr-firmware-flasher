b57a5ff58498ca0d9d5c6a1a2c590bf7b9197db7
Ognjen Stanisavljevic <ognjen.it@gmail.com>
2024-07-09 00:10:55 +0200

    Init added serbian
    Signed-off-by: Ognjen Stanisavljevic <ognjen.it@gmail.com>

---
+++ i18n/Serbian.ts
@@ -0,0 +1,286 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="sr" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../airrohr-flasher.py" line="62"/>
+        <source>Loading firmware list...</source>
+        <translation>Učitavanje liste firmvera...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="277"/>
+        <source>No boards found</source>
+        <translation>Uređaji nisu pronađeni</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="277"/>
+        <source>Have you installed &lt;a href=&quot;{drivers_url}&quot;&gt;the drivers&lt;/a&gt;?</source>
+        <translation>Da li ste instalirali &lt;a href=&quot;{drivers_url}&quot;&gt;draјvere&lt;/a&gt;?</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="286"/>
+        <source>Others...</source>
+        <translation>Ostali...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="576"/>
+        <source>No device selected.</source>
+        <translation>Nije izabran uređaj</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="497"/>
+        <source>No version selected.</source>
+        <translation>Nije izabrana verzija</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="517"/>
+        <source>Invalid version / file does not exist</source>
+        <translation>Nevažeća verzija ili fajl ne postoji</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="522"/>
+        <source>Work in progess...</source>
+        <translation>Rad u toku...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="551"/>
+        <source>Downloading...</source>
+        <translation>Preuzimanje...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="592"/>
+        <source>Connecting...</source>
+        <translation>Povezivanje...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="597"/>
+        <source>Connected. Chip type: {chip_type}</source>
+        <translation>Povezano. Tip čipa: {chip_type}</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="568"/>
+        <source>Erasing complete!</source>
+        <translation>Brisanje završeno!</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="580"/>
+        <source>Erasing in progress...</source>
+        <translation>Brisanje u toku...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="615"/>
+        <source>Writing at 0x{address:08x}...</source>
+        <translation>Pišem na 0x{address:08x}...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="626"/>
+        <source>Finished in {time:.2f} seconds. Sensor ID: {sensor_id}</source>
+        <translation>Završeno za {time:.2f} sekundi. ID senzora: {sensor_id}</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="369"/>
+        <source>Sensor.Community - Airrohr Flasher (v{version})</source>
+        <translation>Sensor.Community - Airrohr Flasher (v{version})</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="371"/>
+        <source>Firmware version:</source>
+        <translation>Verzija firmvera:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="375"/>
+        <source>Upload</source>
+        <translation>Postavi</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="390"/>
+        <source>Erase Flash</source>
+        <translation>Obriši Flash</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="376"/>
+        <source>Expert mode</source>
+        <translation>Ekspertski režim</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="370"/>
+        <source>Board:</source>
+        <translation>Ploča:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="377"/>
+        <source>Baudrate:</source>
+        <translation>Baudrate:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="378"/>
+        <source>Flashing</source>
+        <translation>Flešovanje</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="392"/>
+        <source>Double-click to open configuration page.</source>
+        <translation>Dvostruki klik za otvaranje stranice za konfiguraciju.</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="393"/>
+        <source>Sensors detected in local network:</source>
+        <translation>Senzori otkriveni u lokalnoj mreži:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="394"/>
+        <source>Refresh</source>
+        <translation>Osveži</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="395"/>
+        <source>Discovery</source>
+        <translation>Otkriće</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="396"/>
+        <source>In case of sensor issues, Serial Monitor can be used to review logs sent by the sensor over USB cable.</source>
+        <translation>U slučaju problema sa senzorom, Serial Monitor se može koristiti za pregled logova poslatih preko USB kabla.</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="397"/>
+        <source>Connect</source>
+        <translation>Poveži se</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="398"/>
+        <source>Serial Monitor</source>
+        <translation>Serijski Monitor</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="399"/>
+        <source>&lt;b&gt;Sensor.Community Airrohr Flasher&lt;/b&gt;&lt;br/&gt;Build {build_id}</source>
+        <translation>&lt;b&gt;Sensor.Community Airrohr Flasher&lt;/b&gt;&lt;br/&gt;Build {build_id}</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="400"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Developed by &lt;a href=&quot;https://inf.re/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Piotr Dobrowolski&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;This software is released under the terms of MIT license. No warranty is provided.&lt;/p&gt;&lt;p&gt;For newest release see: &lt;a href=&quot;https://d.inf.re/luftdaten/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://d.inf.re/luftdaten/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Razvio &lt;a href=&quot;https://inf.re/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Piotr Dobrowolski&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Ovaj softver je izdat pod MIT licencom. Bez garancije.&lt;/p&gt;&lt;p&gt;Za najnovije izdanje pogledajte: &lt;a href=&quot;https://d.inf.re/luftdaten/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://d.inf.re/luftdaten/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="401"/>
+        <source>About</source>
+        <translation>O aplikaciji</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="342"/>
+        <source>Invalid sensor name.</source>
+        <translation>Neispravno ime senzora.</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="361"/>
+        <source>Invalid language.</source>
+        <translation>Neispravan jezik.</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="365"/>
+        <source>No SSID typed.</source>
+        <translation>SSID nije unet.</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="373"/>
+        <source>2 times the same sensor.</source>
+        <translation>2 puta isti senzor.</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="395"/>
+        <source>Created invalid json.</source>
+        <translation>Kreiran je neispravan JSON.</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="399"/>
+        <source>Created valid json.</source>
+        <translation>Kreiran je ispravan JSON.</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="402"/>
+        <source>Opening temporary json directory.</source>
+        <translation>Otvaranje privremenog JSON direktorijuma.</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="405"/>
+        <source>Write json in temporay json directory.</source>
+        <translation>Pisanje JSON-a u privremeni JSON direktorijum.</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="409"/>
+        <source>Make SPIFFS bin</source>
+        <translation>Napraviti SPIFFS bin</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="424"/>
+        <source>spiffs.bin done!</source>
+        <translation>spiffs.bin gotov!</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="369"/>
+        <source>No password typed.</source>
+        <translation>Nije uneta lozinka.</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="389"/>
+        <source> </source>
+        <translation> </translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="373"/>
+        <source>Progress:</source>
+        <translation>Napredak:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="379"/>
+        <source>Custom name:</source>
+        <translation>Prilagođeno ime:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="380"/>
+        <source>WiFi SSID:</source>
+        <translation>WiFi SSID:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="381"/>
+        <source>WiFi password:</source>
+        <translation>WiFi lozinka:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="382"/>
+        <source>Sensor 1:</source>
+        <translation>Senzor 1:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="383"/>
+        <source>Sensor 2:</source>
+        <translation>Senzor 2:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="384"/>
+        <source>Language:</source>
+        <translation>Jezik:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="386"/>
+        <source>Save configuration</source>
+        <translation>Sačuvaj konfiguraciju</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="387"/>
+        <source>Configure</source>
+        <translation>Konfiguriši</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="388"/>
+        <source>This will completely erase the flash memory (firmware and configuration). After that you will need to reflash the firmware and reconfigure the device.</source>
+        <translation>Ovo će potpuno obrisati fleš memoriju (firmver i konfiguraciju). Nakon toga ćete morati ponovo da flešujete firmver i rekonfigurišete uređaj.</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="391"/>
+        <source>Erase flash</source>
+        <translation>Obriši fleš</translation>
+    </message>
+</context>
+</TS>
